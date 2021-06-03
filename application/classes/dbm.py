b1cc1d702d5a6ad81c0092406f3a6c73a41ad077
David Harcombe <davidharcombe@google.com>
2021-06-03 15:54:23 -0400

    FR/Bugfix: stop DV360 reports from running multiple times
    - dbm.py
      - If the most recent report's state is 'RUNNING', do NOT start
        the report again.
    
    Change-Id: Id62216c83fac025f4997078447ce99d72ae9396a

--- application/classes/dbm.py
+++ application/classes/dbm.py
@@ -39,7 +39,7 @@ class DBM(ReportFetcher, Fetcher):
   report_type = Type.DV360
   service_definition = Service.DV360
 
-  def __init__(self, email: str, project: str, profile: str=None) -> DBM:
+  def __init__(self, email: str, project: str, profile: str = None) -> DBM:
     self.email = email
     self.project = project
 
@@ -50,11 +50,11 @@ class DBM(ReportFetcher, Fetcher):
       result (Dict): the list of reports for the current user.
     """
     result = self.fetch(
-      self.service.queries().listqueries,
-      **{ 'fields':
-            ('queries(metadata(googleCloudStoragePathForLatestReport,'
-             'latestReportRunTimeMs,title),params/type,queryId,'
-             'schedule/frequency)')}
+        self.service.queries().listqueries,
+        **{'fields':
+           ('queries(metadata(googleCloudStoragePathForLatestReport,'
+            'latestReportRunTimeMs,title),params/type,queryId,'
+            'schedule/frequency)')}
     )
 
     return result
@@ -70,16 +70,16 @@ class DBM(ReportFetcher, Fetcher):
     """
     report = {}
     if all_results := self.fetch(self.service.reports().listreports,
-                                 **{ 'queryId': report_id }):
+                                 **{'queryId': report_id}):
       if 'reports' in all_results:
         # filter out any still running reports or ones with no 'finishTimeMs'
         results = \
-          filter(lambda item: 'finishTimeMs' in \
-                              item.get('metadata', {}).get('status', {}),
-                              all_results['reports'])
+            filter(lambda item: 'finishTimeMs' in
+                   item.get('metadata', {}).get('status', {}),
+                   all_results['reports'])
         ordered = \
-          sorted(results,
-                 key=lambda k: int(k['metadata']['status']['finishTimeMs']))
+            sorted(results,
+                   key=lambda k: int(k['metadata']['status']['finishTimeMs']))
         report = ordered[-1]
       else:
         logging.info('No reports - has this report run successfully yet?')
@@ -100,35 +100,35 @@ class DBM(ReportFetcher, Fetcher):
       result (Dict): the normalized data structure
     """
     query_object = \
-      self.service.queries().getquery(queryId=report_id).execute()
+        self.service.queries().getquery(queryId=report_id).execute()
 
     # Check if report has ever completed a run
     try:
       gcs_path = \
-        query_object['metadata']['googleCloudStoragePathForLatestReport']
+          query_object['metadata']['googleCloudStoragePathForLatestReport']
       latest_runtime = query_object['metadata']['latestReportRunTimeMs']
     except KeyError:
       gcs_path = ''
       latest_runtime = 0
 
     report_data = {
-      'id': query_object['queryId'],
-      'name': query_object['metadata']['title'],
-      'report_name':
-          csv_helpers.sanitize_string(query_object['metadata']['title']),
-      'type': query_object['params']['type'],
-      'current_path': gcs_path,
-      'last_updated':
-          datetime.datetime.fromtimestamp(
+        'id': query_object['queryId'],
+        'name': query_object['metadata']['title'],
+        'report_name':
+        csv_helpers.sanitize_string(query_object['metadata']['title']),
+        'type': query_object['params']['type'],
+        'current_path': gcs_path,
+        'last_updated':
+        datetime.datetime.fromtimestamp(
             float(latest_runtime) / 1000.0).strftime("%Y%m%d%H%M"),
-      'update_cadence': query_object.get('schedule', {}).get('frequency'),
+        'update_cadence': query_object.get('schedule', {}).get('frequency'),
     }
 
     return report_data
 
   @decorators.retry(Exception, tries=3, delay=15, backoff=2)
   def run_report(self, report_id: int,
-                 asynchronous: bool=True) -> Dict[str, Any]:
+                 asynchronous: bool = True) -> Dict[str, Any]:
     """Runs a report on the product.
 
     Args:
@@ -139,21 +139,26 @@ class DBM(ReportFetcher, Fetcher):
         Dict[str, Any]: the run result
     """
     result = {}
-    request = self.service.queries().runquery(queryId=report_id,
-                                              asynchronous=asynchronous)
-    result = request.execute()
+
+    if self.report_state(report_id=report_id) == 'RUNNING':
+      logging.info('Report %s already running.', report_id)
+
+    else:
+      result = \
+          self.service.queries().runquery(queryId=report_id,
+                                          asynchronous=asynchronous).execute()
 
     return result
 
   @decorators.retry(Exception, tries=3, delay=15, backoff=2)
-  def report_state(self, report_id: int):
+  def report_state(self, report_id: int) -> str:
     request = self.service.reports().listreports(queryId=report_id)
     results = request.execute()
 
     if reports := results.get('reports'):
       ordered = \
-        sorted(reports,
-               key=lambda k: int(k['metadata']['reportDataStartTimeMs']))
+          sorted(reports,
+                 key=lambda k: int(k['metadata']['reportDataStartTimeMs']))
       return ordered[-1]['metadata']['status']['state']
 
     else:
@@ -197,14 +202,14 @@ class DBM(ReportFetcher, Fetcher):
     out_file = io.BytesIO()
 
     streamer = \
-      ThreadedGCSObjectStreamUpload(
-        client=Cloud_Storage.client(),
-        creds=credentials.Credentials(
-          email=self.email, project=self.project).get_credentials(),
-        bucket_name=bucket,
-        blob_name=f'{report_id}.csv',
-        chunk_size=chunk_size,
-        streamer_queue=queue)
+        ThreadedGCSObjectStreamUpload(
+            client=Cloud_Storage.client(),
+            creds=credentials.Credentials(
+                email=self.email, project=self.project).get_credentials(),
+            bucket_name=bucket,
+            blob_name=f'{report_id}.csv',
+            chunk_size=chunk_size,
+            streamer_queue=queue)
     streamer.start()
 
     with closing(urlopen(report_details.current_path)) as _report:
